61d07d5a76c1adcb6803e3649b66ba5547f96bb2
ukyuv <yuvarajkh07@gmail.com>
2025-10-04 23:08:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) => {
+      setJokes(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+  })
+
+  return (
+    <>
+      <h1> frontend Backend Integration</h1>
+      <p>Jokes : {jokes.length}</p>
+      {
+        // jokes.map((joke, index) => {
+        //   <div key={joke.index}>
+        //     <h3>{joke.title}</h3>
+        //     <p>{joke.content}</p>
+        //   </div>
+        // })
+        jokes.map((joke, index) => (
+          <div key={joke.index}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+        
+        
+      }
+    </>
+  )
+}
+
+export default App
